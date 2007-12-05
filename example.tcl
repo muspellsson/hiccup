c5f469fd2dc55ad17c8dd3d4881a77bf7b201e30
consalus <consalus@1f30393f-d42e-0410-812c-574253b2cdb5>
2007-12-05 19:19:32 +0000

    Fixed foreach implementation.

--- example.tcl
+++ example.tcl
@@ -35,7 +35,7 @@ puts "\nDone."
 proc foreach {vname lst what} {
   set i 0
   while {< $i [llength $lst]} {
-    uplevel "set $vname [lindex $lst $i]"
+    uplevel "set $vname {[lindex $lst $i]}"
     uplevel $what
     incr i
   }
