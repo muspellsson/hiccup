381407a6af58d843e83137ac779488f3d814303f
consalus <consalus@1f30393f-d42e-0410-812c-574253b2cdb5>
2008-04-24 22:15:12 +0000

    Added little expr comparision script.

---
+++ compare_expr.tcl
@@ -0,0 +1,32 @@
+
+
+puts "expr:"
+puts [time { set x 0; while { expr { ($x + 1) < 15 * 20 + 5 } } { incr x } } 100]
+
+puts "expr memo:"
+set code { ($x + 1) < 15 * 20 + 5 } 
+puts [time { set x 0; while { expr $code } { incr x } } 100]
+
+puts "eval:"
+puts [time { set x 0; while { eval { < [+ 1 $x]  [+ [* 15 20] 5] } } { incr x } } 100]
+
+set x 11
+puts "expr simple:"
+puts [time { expr { $x <= 4 } } 20000]
+
+puts "eval simple:"
+puts [time { eval { <= $x 4 } } 20000]
+
+exit
+
+set z 0
+while { bsexpr { $z < 100 } } {
+  set y 0
+  while { bsexpr { $y < 10 + (1 * 1 + 20 * 31 - 31 * 20) } } {
+    set x 0; while { bsexpr { ($x + 1) < 10 * 20 + 5 } } { incr x } 
+    incr y
+  }
+  incr z
+}
+exit
+
