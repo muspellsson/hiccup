340c7ee8b29cf32b7fba30eb8980d33695e58307
consalus <consalus@1f30393f-d42e-0410-812c-574253b2cdb5>
2008-05-13 00:34:31 +0000

    Upped inlining threshold.

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ fi
 if [[ $1 == "prof" ]]; 
   then ghc -O2 -fglasgow-exts -o hiccup Main.hs -funbox-strict-fields --make -prof -auto-all;
 else
-ghc -O2 -fvia-c -optc-O3 -fglasgow-exts -o hiccup Main.hs -funbox-strict-fields -funfolding-use-threshold=16 --make $EXTRA;
+ghc -O2 -fvia-c -optc-O3 -fglasgow-exts -o hiccup Main.hs -funbox-strict-fields -funfolding-use-threshold=24 --make $EXTRA;
 fi
