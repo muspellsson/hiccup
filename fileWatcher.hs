fd14f9d06a3a678fea3784061c14ffb58cb75450
consalus <consalus@1f30393f-d42e-0410-812c-574253b2cdb5>
2008-06-27 22:12:18 +0000

    Fixed up fileWatcher, now using it in continuous.

--- fileWatcher.hs
+++ fileWatcher.hs
@@ -1,26 +1,28 @@
 import Control.Concurrent
 import Control.Monad
+import System.Process
 import System.Posix.Files
-import System.Posix.Signals ()
+import Text.Printf
 import System
 import qualified Control.Exception as E
 
 main = do
   files <- getArgs
-  E.catch (watchLoop 0 files) print
+  putStrLn $ "Watching " ++ unwords files
+  E.catch (watcher files) print
 
-sleep n = threadDelay ((n * 1000000))
 
-watchLoop latest files = do
+watcher files = watchLoop 0 where  
+ watchLoop latest = do
   nlatest <- latestMod files
   if nlatest > latest
     then do
-      when (latest > 0) $ print (nlatest - latest)
-      putStrLn $ "Files: " ++ show files
-      watchLoop nlatest files
-    else do sleep 3
-            watchLoop latest files
+      when (latest > 0) $ putStrLn $ show (nlatest - latest) ++ "s"
+      evalCmd "sh ./test.sh"
+      watchLoop nlatest 
+    else sleep 3 >> watchLoop latest
    
 latestMod fnl = mapM modTime fnl >>= return . maximum
-
 modTime fn = getFileStatus fn >>= return . modificationTime
+evalCmd c = runCommand c >>= waitForProcess
+sleep n = threadDelay ((n * 1000000))
