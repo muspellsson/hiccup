5ac09e486e34e43475ab9bd15972caf1bcbf0daf
consalus <consalus@1f30393f-d42e-0410-812c-574253b2cdb5>
2008-08-21 01:50:14 +0000

    Fast build.

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ if [[ $1 == "prof" ]];
   then ghc -O2 -fglasgow-exts -o hiccup Main.hs -funbox-strict-fields --make -prof -auto-all;
 elif [[ $1 == "simp" ]];
  then ghc -O2 -fglasgow-exts Core.hs -funbox-strict-fields -funfolding-use-threshold=24 -ddump-simpl --make $EXTRA > core-simp.hs;
-elif [[ $1 == "slow" ]]; 
-  then ghc -O0 -fglasgow-exts -o hiccup Main.hs -funbox-strict-fields --make -auto-all;
+elif [[ $1 == "fast" ]]; 
+  then ghc -O0 -fglasgow-exts -o hiccup Main.hs -funbox-strict-fields --make -no-recomp;
 else
 ghc -O2 -fvia-c -optc-O3 -fglasgow-exts -o hiccup Main.hs -funbox-strict-fields -funfolding-use-threshold=24 --make $EXTRA;
 fi
